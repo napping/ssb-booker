d5e84d403ea44ddf8095844f484443088da57ffb
Brian Shi <brian.shi@lighthouseworld.com>
2023-06-04 13:38:41 +0800

    Update scaffolding

--- my-app/src/pages/Home/index.tsx
+++ my-app/src/pages/Home/index.tsx
@@ -18,48 +18,15 @@ const Home = () => {
   return (
     <Container>
       <ScrollToTop />
-      <ContentBlock
-        type="right"
-        title={IntroContent.title}
-        content={IntroContent.title}
-        button={IntroContent.button}
-        icon="developer.svg"
-        id="intro"
+     
+      <Wizard
       />
-      <MiddleBlock
-        title={MiddleBlockContent.title}
-        content={MiddleBlockContent.text}
-        button={MiddleBlockContent.button}
-      />
-      <ContentBlock
-        type="left"
-        title={AboutContent.title}
-        content={AboutContent.text}
-        section={AboutContent.section}
-        icon="graphs.svg"
-        id="about"
-      />
-      <ContentBlock
-        type="right"
-        title={MissionContent.title}
-        content={MissionContent.text}
-        icon="product-launch.svg"
-        id="mission"
-      />
-      <ContentBlock
-        type="left"
-        title={ProductContent.title}
-        content={ProductContent.text}
-        icon="waving.svg"
-        id="product"
-      />
+
       <Contact
         title={ContactContent.title}
         content={ContactContent.text}
         id="contact"
       />
-
-      <Wizard />
 
     </Container>
   );
